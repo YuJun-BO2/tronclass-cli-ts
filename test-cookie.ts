1bd3e53b03d1605f441e2dd3ba2486a6463b70d9
Huang Yu Jun <yujun@bo2.tw>
2026-04-18 00:36:36 +0800

    chore: remove unused test-cookie.ts file

--- test-cookie.ts
+++
@@ -1,7 +0,0 @@
-import { CookieJar } from "tough-cookie";
-const jar = new CookieJar();
-jar.setCookieSync("a=b; Domain=example.com", "http://example.com");
-const json = jar.toJSON();
-console.log(JSON.stringify(json));
-const jar2 = CookieJar.fromJSON(json);
-console.log(jar2.getCookieStringSync("http://example.com"));
